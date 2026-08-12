0d327a11032f510c7a6f9debe5796e345c605e43
linghypshen@gmail.com <linghypshen@gmail.com>
2026-08-12 10:59:39 -0400

    feat(intent): evaluate thresholds and promotion gates

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -13,3 +13,16 @@ from .intent_data import IntentDatasetSplit as IntentDatasetSplit
 from .intent_data import IntentExample as IntentExample
 from .intent_data import load_intent_examples as load_intent_examples
 from .intent_data import split_intent_examples as split_intent_examples
+from .intent_evaluation import IntentEvaluationReport as IntentEvaluationReport
+from .intent_evaluation import IntentPredictionRecord as IntentPredictionRecord
+from .intent_evaluation import PromotionCriteria as PromotionCriteria
+from .intent_evaluation import PromotionDecision as PromotionDecision
+from .intent_evaluation import (
+    compare_for_promotion as compare_for_promotion,
+)
+from .intent_evaluation import (
+    evaluate_intent_predictions as evaluate_intent_predictions,
+)
+from .intent_evaluation import (
+    select_confidence_threshold as select_confidence_threshold,
+)
